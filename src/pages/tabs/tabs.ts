a48d2f1c0611760b4f407cdfc4f5f5150b3c53fd
SkyHeaven <yohann.mateo@gmail.com>
2018-03-28 14:20:05 +0200

    Propre

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -5,20 +5,18 @@ import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 import {TodosPage} from "../todos/todos";
 import {ProfilePage} from "../profile/profile";
+import {AdMobFree, AdMobFreeBannerConfig} from "@ionic-native/admob-free";
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
-  tab4Root = TodosPage;
-  tab5Root = ProfilePage;
+  tab1Root = TodosPage;
+  tab2Root = ProfilePage;
 
-  constructor() {
-
+  constructor(private admobFree: AdMobFree) {
+    this.displayBanner();
   }
 
   ionViewDidEnter() {
@@ -28,6 +26,25 @@ export class TabsPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TabsPage');
+  }
+
+  displayBanner() {
+    const bannerConfig: AdMobFreeBannerConfig = {
+      // we will just use a test id for this tutorial
+      //id: 'ca-app-pub-3314972172145285/3705739439',
+      isTesting: true,
+      autoShow: true,
+      bannerAtTop:false // default is false
+    };
+
+    this.admobFree.banner.config(bannerConfig);
+
+    this.admobFree.banner.prepare().then((result)=>{
+      console.log(result);
+    },(reason)=>{
+      console.log(reason);
+    });
+
   }
 
 }
